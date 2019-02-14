b884816159f97d6844bf9a53f6142344b0d2380b
Alex Kuiper <iamalex7517@gmail.com>
2019-02-14 18:29:51 -0500

    Created a Frame that will ask the user for authentication prior to opening the maintenance frame.

--- src/com/granolaBars/Main.java
+++ src/com/granolaBars/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         mainFrame.setVisible(true);
+        MaintenanceFrame maintenanceFrame = new MaintenanceFrame();
+        maintenanceFrame.setVisible(true);
+        AuthenticationFrame authenticationFrame = new AuthenticationFrame();
+        authenticationFrame.setVisible(true);
     }
 }
