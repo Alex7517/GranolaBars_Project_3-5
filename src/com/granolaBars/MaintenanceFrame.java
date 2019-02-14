de357f5ac8b9ed82b954f012ae4bf6a08697fd6e
KritsiaF <kritsiafigueroa@yahoo.com>
2019-02-14 15:47:56 -0500

    done

--- src/com/granolaBars/MaintenanceFrame.java
+++ src/com/granolaBars/MaintenanceFrame.java
@@ -2,9 +2,13 @@ package com.granolaBars;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 
 public class MaintenanceFrame extends JFrame{
      private JLabel MaintenanceFormHeader;
+     private JLabel FileNameLabel;
+     private JLabel StatusLabel;
      private JButton AddFileButton;
      private JButton RebuildButton;
      private JButton RemoveSelectedFilesButton;
@@ -27,23 +31,36 @@ public class MaintenanceFrame extends JFrame{
         MaintenanceFormHeader.setSize(500, 50);
         MaintenanceFormHeader.setFont(new Font("Calibri", Font.BOLD, 30));
         add(MaintenanceFormHeader);
+        
+        // File name label
+        FileNameLabel = new JLabel("File Name");
+        FileNameLabel.setLocation(175, 50);
+        FileNameLabel.setSize(250, 20);
+        FileNameLabel.setFont(new Font("Calibri", Font.PLAIN, 16));
+        add(FileNameLabel);
+        
+        StatusLabel = new JLabel("Status");
+        StatusLabel.setLocation(475, 50);
+        StatusLabel.setSize(250, 20);
+        StatusLabel.setFont(new Font("Calibri", Font.PLAIN, 16));
+        add(StatusLabel);
        
         //Add file button
         AddFileButton = new JButton("Add File");   
         AddFileButton.setSize(100, 30);
-        AddFileButton.setLocation(75, 375);
+        AddFileButton.setLocation(75, 390);
         add(AddFileButton);
    
         //Add rebuild button
         RebuildButton = new JButton("Rebuild Out-Of-Date");   
         RebuildButton.setSize(150, 30);
-        RebuildButton.setLocation(250, 375);
+        RebuildButton.setLocation(250, 390);
         add(RebuildButton);
         
         //Add remove selected files button
         RemoveSelectedFilesButton = new JButton("Remove Selected Files");   
         RemoveSelectedFilesButton.setSize(175, 30);
-        RemoveSelectedFilesButton.setLocation(475, 375);
+        RemoveSelectedFilesButton.setLocation(475, 390);
         add(RemoveSelectedFilesButton);
     
         //Add reset windows button
