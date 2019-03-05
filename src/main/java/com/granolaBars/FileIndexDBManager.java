dcdcae63a857d808a3ee1bbb58d8e81a1dcbd498
Alex Kuiper <iamalex7517@gmail.com>
2019-03-05 15:53:18 -0500

    Fixed a testDB method name error and created try blocks for the DB managers methods

--- src/main/java/com/granolaBars/FileIndexDBManager.java
+++ src/main/java/com/granolaBars/FileIndexDBManager.java
@@ -11,35 +11,48 @@ public class FileIndexDBManager {
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory( "FileIndexDB" );
     static EntityManager em = emf.createEntityManager();
 
-    static void testBD(){
+    static void testDB(){
         createFile("I am a Test Path: C:Place", new Date());
         createFile("I am another Test Path: C:Place2", new Date());
-        System.out.println(loadFile("SELECT file FROM IndexedFile file"));
+        System.out.println(loadFile("SELECT file FROM IndexedFile"));
     }
 
     //This will create a file to be added to the DB, then adds it
     //This is a partial stub Method
     static boolean createFile(String path, Date lastModification){
-        //This creates a IndexedFile to be added to the DB
-        IndexedFile file;
-        file = new IndexedFile();
-        file.setPath(path);
-        file.setLastModification(lastModification);
+        try{
+            //This creates a IndexedFile to be added to the DB
+            IndexedFile file;
+            file = new IndexedFile();
+            file.setPath(path);
+            file.setLastModification(lastModification);
 
-        //This adds the file to the DB
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        em.persist(file);
-        tx.commit();
-        return true;
+            //This adds the file to the DB
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            em.persist(file);
+            tx.commit();
+            return true;
+        }
+        catch (RuntimeException e){
+            return false;
+        }
     }
 
     //This will create a quarry with a passed SQL statement, then return results in a list
     //This is a partial stub Method
     static List<IndexedFile> loadFile(String sqlStatements){
-        Query getAllFiles = em.createQuery(sqlStatements);
         @SuppressWarnings("unchecked")
-        List<IndexedFile> lndexedFileReturn = getAllFiles.getResultList();
+        List<IndexedFile> lndexedFileReturn;
+        try{
+            Query getAllFiles = em.createQuery(sqlStatements);
+            lndexedFileReturn = getAllFiles.getResultList();
+        }
+        catch (IllegalArgumentException e)
+        {
+            lndexedFileReturn = null;
+            System.out.println(e);
+        }
         return lndexedFileReturn;
     }
 }
