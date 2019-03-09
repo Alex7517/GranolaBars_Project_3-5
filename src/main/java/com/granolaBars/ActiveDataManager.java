74345543a911a3b6eecea51329bddfe03869eea9
Alex Kuiper <iamalex7517@gmail.com>
2019-03-09 11:24:47 -0500

    Updated the active and persistent data manager, persistent is mostly done and active shows workable examples

--- src/main/java/com/granolaBars/ActiveDataManager.java
+++ src/main/java/com/granolaBars/ActiveDataManager.java
@@ -1,21 +1,46 @@
 package com.granolaBars;
 
+import java.io.IOException;
 import java.util.*;
 
 public class ActiveDataManager {
     static void test(){
         //This creates a map of example IDs
-        Map<Integer, String> i = new Hashtable<>();
-        i.put(1,"ID DATA 1");
-        i.put(2,"ID DATA 2");
-        i.put(3,"ID DATA 3");
+        Map<Integer, String[]> i = new Hashtable<>();
+
+        //This finds a path
+        String path;
+        try{
+            path = new java.io.File( "." ).getCanonicalPath();
+
+        }
+        catch (IOException e){
+            path = "Path not found";
+        }
+
+        //This finds the Date --We will likely want to switch to a calender object
+        //Then it saves the path and the date to the String array
+        String[] tempS= {path,new Date().toString()};
+
+        //This adds the String array to the map --Imagine each being a new file and the int to be the ID for each
+        i.put(1,tempS);
+        i.put(2,tempS);
+        i.put(3,tempS);
 
         //This creates a map of example index
-        Map<Integer, String> j = new Hashtable<>();
-        j.put(1,"Index Data 1");
-        j.put(2,"Index Data 2");
-        j.put(3,"Index Data 3");
-        j.put(4,"Index Data 4");
+        Map<String, List<Integer[]>> j = new Hashtable<>();
+
+        //This creates an list that will hold small arrays, each list item will be associated to a specific word,
+                                                            // and each array will hold the words file ID and POS
+        List<Integer[]> temp = new ArrayList<>();
+        Integer[] array1 = {1,22};
+        Integer[] array2 = {3,52};
+
+        temp.add(array1);
+        temp.add(array2);
+
+        //This adds the list to the specific word
+        j.put("BLUE",temp);
 
         //This calls the method that saves the DATA
         PersistentDataManager.saveData(i,j);
@@ -23,11 +48,21 @@ public class ActiveDataManager {
 
         //This calls for the DATA and separates the DATA
         Map[] dataReturn = PersistentDataManager.loadData();
-        //Map idDATA = dataReturn[0];
-        //Map indexDATA = dataReturn[1];
+        //ID
+        Map<Integer, String[]> idDATA = dataReturn[0];
+        //Index
+        Map<String, List<Integer[]>> indexDATA = dataReturn[1];
 
-        //
-        //System.out.println(idDATA.get(1));
-        //System.out.println(indexDATA.get(1));
+        //This is the path
+        System.out.println(idDATA.get(1)[0]);
+        //This is the date
+        System.out.println(new Date(idDATA.get(1)[1]));
+
+        //This returns the list for a specific word
+        System.out.println(indexDATA.get("BLUE"));
+        //This returns the file Id for the first Blue word
+        System.out.println(indexDATA.get("BLUE").get(0)[0]);
+        //This returns the POS for the first Blue word
+        System.out.println(indexDATA.get("BLUE").get(0)[1]);
     }
 }
--- src/main/java/com/granolaBars/PersistentDataManager.java
+++ src/main/java/com/granolaBars/PersistentDataManager.java
@@ -7,6 +7,7 @@ import org.json.*;
 
 //This is only needed to actually save and load the string
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 
 public class PersistentDataManager {
@@ -14,28 +15,28 @@ public class PersistentDataManager {
     final static private String ID = "IDs";
     final static private String INDEX = "Index";
 
-    static void saveData(Map idData,Map indexData){
+    static void saveData(Map<Integer,String[]> idData,Map<String, List<Integer[]>> indexData){
 
         //This creates a object that contains the DATA and manages the conversion from real DATA to strings
-        JSONObject dataObj = new JSONObject();
+        JSONObject idDataObj = new JSONObject();
+        JSONObject indexDataObj = new JSONObject();
 
-        //This is an example of the DATA to be saved, its like a map saving maps.
-        //At the moment the map is a string but this is just for example, in the future it will be a map object.
-        //The IDs key will point to the value containing our Map object that holds the META info, think of the DATA in the DB now.
-        //The Index key will point to the value containing our Map object that holds the inverted index
-        dataObj.put(ID,idData);
-        dataObj.put(INDEX,indexData);
+        //This saves the DATA to the objects
+        idDataObj.put(ID,idData);
+        indexDataObj.put(INDEX,indexData);
 
-        //DIAG This shows that the JSONObject is just being converted to a string normally.
-        System.out.println(dataObj);
+        //DEBUG This shows that the JSONObject is just being converted to a string normally.
+        System.out.println(idDataObj);
+        System.out.println(indexDataObj);
 
-        //This actually creates the file that saves the persistent DATA.
+        //This creates the file that saves the persistent DATA.
         //It creates the file if not found, and replaces it if it already exists.
         try{
             //This creates the text output steam
             PrintStream out = new PrintStream( new FileOutputStream( FILENAME ) );
-            //This actually saves the file
-            out.println( dataObj );
+            //This actually saves the DATA to the file
+            out.println( idDataObj );
+            out.println( indexDataObj );
             //This closes the file to prevent errors
             out.close();
         }
@@ -54,24 +55,22 @@ public class PersistentDataManager {
             //This creates the text input steam
             BufferedReader in = new BufferedReader( new FileReader( FILENAME ) );
             //This holds the string pulled from the file
-            String dataString = in.readLine();
+            String idDataString = in.readLine();
+            String indexDataString = in.readLine();
             //This closes the file to prevent errors
             in.close();
 
-            //This converts the DATA back from a string into a JSON file that we can access
-            JSONObject dataObj = new JSONObject( dataString );
-
-            //DIAG This is just for example, later this would be something like: Map idMap = newObj.get("IDs");
-            System.out.println(dataObj.get(ID));
-            //System.out.println(dataObj.get(INDEX));
-
             //This converts the DATA back from a string into a GSON file, so that it can be converted into a Map
-            //java.lang.reflect.Type mapType = new TypeToken<Map<String, Map<Integer, String>>>(){}.getType();
-            //Map<String, Map> idData = new Gson().fromJson(dataObj.get(ID).toString(),mapType);
+            //IDS
+            java.lang.reflect.Type idDataObjType = new TypeToken<Map<String, Map<Integer,String[]>>>(){}.getType();
+            Map<String, Map> idDataObj = new Gson().fromJson(idDataString,idDataObjType);
+            //INDEX
+            java.lang.reflect.Type indexDataObjType = new TypeToken<Map<String, Map<String, List<Integer[]>>>>(){}.getType();
+            Map<String, Map> indexDataObj = new Gson().fromJson(indexDataString,indexDataObjType);
 
             //This adds the data to a returned array
-            //dataReturn[0]= idData;
-            //dataReturn[1]= dataObj.get(INDEX);
+            dataReturn[0] = (Map<Integer, String[]>)idDataObj.get(ID);
+            dataReturn[1] = (Map<String, List<Integer[]>>)indexDataObj.get(INDEX);
         }
         //FileNotFoundException is a checked error from the FileOutputStream method above.
         catch (FileNotFoundException e){
