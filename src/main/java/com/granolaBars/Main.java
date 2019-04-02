6be8bb88e604e6a8e885c85459f1dd06c31889ce
Alex Kuiper <iamalex7517@gmail.com>
2019-04-02 14:55:28 -0400

    Cleaned up main

--- src/main/java/com/granolaBars/Main.java
+++ src/main/java/com/granolaBars/Main.java
@@ -1,19 +1,15 @@
 package com.granolaBars;
 
-import java.util.ArrayList;
-
 public class Main {
     static ActiveDataManager activeDataManager;
     static MainFrame mainFrame;
     static MaintenanceFrame maintenanceFrame;
 
     public static void main(String[] args) {
-        ArrayList<updatableGUI> GUI = new ArrayList<>();
         mainFrame = new MainFrame();
         maintenanceFrame = new MaintenanceFrame();
-        GUI.add(maintenanceFrame);
         activeDataManager = new ActiveDataManager();
-        activeDataManager.addDisplayGUI(GUI);
+        activeDataManager.addDisplayGUI(maintenanceFrame);
         mainFrame.setVisible(true);
     }
 }
